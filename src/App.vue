bf9968b1ef038738ac8fcd0b6d25386b2799668c
Alexander Vödisch <voedisch.alexander@gmail.com>
2023-11-22 19:13:24 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useLocalStorage } from '@vueuse/core'
+
+const state = useLocalStorage('state', { count: 0 })
+</script>
+
+<template>
+  <div>
+    <button @click="state.count += 1">
+      inc
+    </button>
+    {{ state }}
+  </div>
+</template>
+
+<style>
+</style>
