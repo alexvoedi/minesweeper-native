61cc524635dc186c037f74f95b77bddd22411162
Alexander Vödisch <voedisch.alexander@gmail.com>
2023-11-23 10:30:24 +0100

    feat: basic implementation

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,77 @@
 <script setup lang="ts">
 import { useLocalStorage } from '@vueuse/core'
-import { computed, ref } from 'vue';
+import { computed, ref } from 'vue'
 import Cell from '@/components/Cell.vue'
 
 enum Difficulty {
-	Beginner = 'beginner',
-	Intermediate = 'intermediate',
-	Expert = 'expert'
+  Beginner = 'beginner',
+  Intermediate = 'intermediate',
+  Expert = 'expert',
 }
 
 const difficulties = {
-	[Difficulty.Beginner]: {
-		rows: 9,
-		columns: 9,
-		mines: 10
-	},
-	[Difficulty.Intermediate]: {
-		rows: 16,
-		columns: 16,
-		mines: 40
-	},
-	[Difficulty.Expert]: {
-		rows: 16,
-		columns: 30,
-		mines: 99
-	}
+  [Difficulty.Beginner]: {
+    rows: 9,
+    columns: 9,
+    mines: 10,
+  },
+  [Difficulty.Intermediate]: {
+    rows: 16,
+    columns: 16,
+    mines: 40,
+  },
+  [Difficulty.Expert]: {
+    rows: 16,
+    columns: 30,
+    mines: 99,
+  },
 }
 
 const storage = useLocalStorage('minesweeper', {
-	highscores: {
-		beginner: [],
-		intermediate: [],
-		expert: [],
-	},
-	settings: {
-		difficulty: Difficulty.Beginner,
-		theme: 'light',
-		cellSize: 24,
-	}
+  highscores: {
+    beginner: [],
+    intermediate: [],
+    expert: [],
+  },
+  settings: {
+    difficulty: Difficulty.Beginner,
+    theme: 'light',
+    cellSize: 24,
+  },
 })
-
-const timer = ref(0)
 
 const settings = computed(() => difficulties[storage.value.settings.difficulty])
 
-const openCell = (x: number, y: number) => {
-	console.log('openCell', x, y)
+function openCell(x: number, y: number) {
+
 }
 </script>
 
 <template>
-	<div>
-		<div>
-			<h1>Minesweeper</h1>
-		</div>
+  <div>
+    <div>
+      <h1>Minesweeper</h1>
+    </div>
 
-		<button @click="storage.settings.cellSize += 1">increase</button>
-		<button @click="storage.settings.cellSize -= 1">decrease</button>
+    <button @click="storage.settings.cellSize += 1">
+      increase
+    </button>
+    <button @click="storage.settings.cellSize -= 1">
+      decrease
+    </button>
 
-		<div :style="{
-			display: 'grid',
-			gridTemplateColumns: `repeat(${settings.columns}, ${storage.settings.cellSize}px)`,
-			gridTemplateRows: `repeat(${settings.rows}, ${storage.settings.cellSize}px)`,
-		}">
-			<div v-for="(row, rowIndex) of settings.rows" :key="rowIndex">
-				<div v-for="(column, columnIndex) of settings.columns" :key="columnIndex">
-					<Cell @click="() => openCell(columnIndex, rowIndex)" :cellSize="storage.settings.cellSize" />
-				</div>
-			</div>
-		</div>
-	</div>
+    <div :style="{
+      display: 'grid',
+      gridTemplateColumns: `repeat(${settings.columns}, ${storage.settings.cellSize}px)`,
+      gridTemplateRows: `repeat(${settings.rows}, ${storage.settings.cellSize}px)`,
+    }">
+      <div v-for="(row, rowIndex) of settings.rows" :key="rowIndex">
+        <div v-for="(column, columnIndex) of settings.columns" :key="columnIndex">
+          <Cell :cell-size="storage.settings.cellSize" @click="() => openCell(columnIndex, rowIndex)" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from 'vue'
 
 const props = defineProps<{
-  cellSize: number;
+  cellSize: number
 }>()
 
 enum State {
   Open = 'open',
   Closed = 'closed',
   Flagged = 'flagged',
-  Question = 'question'
+  Question = 'question',
 }
 
 const state = ref(State.Closed)
@@ -18,10 +18,10 @@ const css = {
   [State.Open]: 'bg-gray-400',
   [State.Closed]: 'bg-gray-300',
   [State.Flagged]: 'bg-red-500',
-  [State.Question]: 'bg-yellow-500'
+  [State.Question]: 'bg-yellow-500',
 }
 
-const toggleState = () => {
+function toggleState() {
   switch (state.value) {
     case State.Closed:
       state.value = State.Flagged
@@ -37,13 +37,15 @@ const toggleState = () => {
 </script>
 
 <template>
-  <button class="flex items-center justify-center bg-gray border border-gray-300" :class="[css]" :style="{
-    width: `${props.cellSize}px`,
-    height: `${props.cellSize}px`,
-    fontSize: `${0.75 * props.cellSize}px`
-  }" @contextmenu.prevent="toggleState()">
-    <span v-if="state === State.Flagged" class="ico-mdi-flag text-red-700"></span>
-    <span v-else-if="state === State.Question" class="ico-mdi-help"></span>
+  <button
+    class="flex items-center justify-center bg-gray border border-gray-300" :class="[css]" :style="{
+      width: `${props.cellSize}px`,
+      height: `${props.cellSize}px`,
+      fontSize: `${0.75 * props.cellSize}px`,
+    }" @contextmenu.prevent="toggleState()"
+  >
+    <span v-if="state === State.Flagged" class="ico-mdi-flag text-red-700" />
+    <span v-else-if="state === State.Question" class="ico-mdi-help" />
   </button>
 </template>
 
